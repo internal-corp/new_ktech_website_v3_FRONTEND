b199901cb9244a45a5f9beca5d6831bebd9abb4a
Abraham Obed <65366889+ofceab@users.noreply.github.com>
2021-12-04 11:44:12 +0000

    Added base animation on carousel

--- js/certifical-carousel.js
+++ js/certifical-carousel.js
@@ -8,7 +8,6 @@ let index=2;
         certificatImageBox.classList.toggle('anim-come-from-right');
         const path=`assets/certificats/certificat-${index}.jpg`;
         certificatImageBox.setAttribute('src',path);
-        certificatImageBox.classList.toggle('anim-come-from-right');
         index++;
         if(index>4){
             index=1;
